2b989cffdcac469ff6cfa24caf4afd5152a386bd
Dana Le <danadaole@gmail.com>
2026-04-25 22:35:48 +1000

    implement the ability to download the page

--- scraper.py
+++ scraper.py
@@ -1,1 +1,13 @@
 # scraping logic
+import requests
+from bs4 import BeautifulSoup
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+def scrape_product(url):
+    response = requests.get(url, headers=HEADERS, timeout=10)
+    response.raise_for_status()
+
+
